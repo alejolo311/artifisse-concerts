f3fdc1ca96e6d093fe5e5e838f2a17973ba1658b
alejolo311 <alejolo311@gmail.com>
2020-06-23 00:47:02 -0500

    barebones

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname +'/static/html/index.html');
+});
+app.listen(3000, function(){
+    console.log('listening on *:3000');
+});
